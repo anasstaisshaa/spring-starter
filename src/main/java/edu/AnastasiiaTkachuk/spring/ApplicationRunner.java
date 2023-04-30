f6e5e1455c45b856112c03b234d1868e472ae7f4
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-30 18:47:01 +0300

    add Constructor Injection

--- src/main/java/edu/AnastasiiaTkachuk/spring/ApplicationRunner.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/ApplicationRunner.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        ConnectionPool connectionPool = context.getBean("pool2", ConnectionPool.class);
+        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);
 
     }
