f8fde8216fe8a38ba7a47299eb2ce6319e0860b7
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-30 19:14:45 +0300

    add Factory Method Injection

--- src/main/java/edu/AnastasiiaTkachuk/spring/ApplicationRunner.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package edu.AnastasiiaTkachuk.spring;
 
 import edu.AnastasiiaTkachuk.spring.database.pool.ConnectionPool;
+import edu.AnastasiiaTkachuk.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -8,6 +9,9 @@ public class ApplicationRunner {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);
+
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
 
     }
 }
