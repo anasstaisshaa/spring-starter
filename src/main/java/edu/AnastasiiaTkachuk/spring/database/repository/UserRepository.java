c5cbb574a5f5e3a1046b6299b238de4ee4c3599c
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-02 20:45:13 +0300

    add classpath scanning

--- src/main/java/edu/AnastasiiaTkachuk/spring/database/repository/UserRepository.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/database/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package edu.AnastasiiaTkachuk.spring.database.repository;
 
 import edu.AnastasiiaTkachuk.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepository {
     private final ConnectionPool connectionPool;
 
