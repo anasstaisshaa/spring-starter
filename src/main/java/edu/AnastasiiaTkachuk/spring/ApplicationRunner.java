4921cb2478b9dba58d714c291e2e918868780336
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-02 19:45:11 +0300

    add custom BeanPostProcessor

--- src/main/java/edu/AnastasiiaTkachuk/spring/ApplicationRunner.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/ApplicationRunner.java
@@ -2,18 +2,18 @@ package edu.AnastasiiaTkachuk.spring;
 
 import edu.AnastasiiaTkachuk.spring.database.pool.ConnectionPool;
 import edu.AnastasiiaTkachuk.spring.database.repository.CompanyRepository;
+import edu.AnastasiiaTkachuk.spring.database.repository.CrudRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        CompanyRepository companyRepository;
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
 
             ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
 
-            companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-            System.out.println(companyRepository);
+            CrudRepository companyRepository = context.getBean("companyRepository", CrudRepository.class);
+            System.out.println(companyRepository.findById(1));
         }
     }
 }
--- src/main/java/edu/AnastasiiaTkachuk/spring/database/repository/CompanyRepository.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/database/repository/CompanyRepository.java
@@ -1,15 +1,31 @@
 package edu.AnastasiiaTkachuk.spring.database.repository;
 
+import edu.AnastasiiaTkachuk.spring.database.bpp.InjectBean;
+import edu.AnastasiiaTkachuk.spring.database.bpp.Transaction;
+import edu.AnastasiiaTkachuk.spring.database.entity.Company;
 import edu.AnastasiiaTkachuk.spring.database.pool.ConnectionPool;
 
-public class CompanyRepository {
-    private final ConnectionPool connectionPool;
+import javax.annotation.PostConstruct;
+import java.util.Optional;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
+@Transaction
+public class CompanyRepository implements CrudRepository<Integer, Company>{
+    @InjectBean
+    private ConnectionPool connectionPool;
+
+    @PostConstruct
+    private void init(){
+        System.out.println("Init company repository");
     }
 
-    public static CompanyRepository of(ConnectionPool connectionPool){
-        return new CompanyRepository(connectionPool);
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("findById method...");
+        return Optional.of(new Company(id));
+    }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("delete method...");
     }
 }
