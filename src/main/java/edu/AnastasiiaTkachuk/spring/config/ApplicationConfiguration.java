670a298cb823a9ebefa3bbfbf12180df30548808
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-03 16:22:13 +0300

    add @Import

--- src/main/java/edu/AnastasiiaTkachuk/spring/config/ApplicationConfiguration.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,11 @@
 package edu.AnastasiiaTkachuk.spring.config;
 
 import edu.AnastasiiaTkachuk.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
+import edu.AnastasiiaTkachuk.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
-
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages ="edu.AnastasiiaTkachuk.spring",
---
+++ src/main/java/edu/AnastasiiaTkachuk/web/config/WebConfiguration.java
@@ -0,0 +1,7 @@
+package edu.AnastasiiaTkachuk.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+}
