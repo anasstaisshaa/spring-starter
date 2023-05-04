076e6282206047d8fadac7f05695bbfae6c0a64b
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-04 20:15:18 +0300

    and event and listener

--- src/main/java/edu/AnastasiiaTkachuk/spring/service/CompanyService.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/service/CompanyService.java
@@ -1,12 +1,27 @@
 package edu.AnastasiiaTkachuk.spring.service;
 
+import edu.AnastasiiaTkachuk.spring.database.entity.Company;
+import edu.AnastasiiaTkachuk.spring.database.repository.CompanyRepository;
+import edu.AnastasiiaTkachuk.spring.database.repository.CrudRepository;
+import edu.AnastasiiaTkachuk.spring.dto.CompanyReadDto;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
+    private final CrudRepository<Integer, Company> companyRepository;
     private final UserService userService;
 
-    public CompanyService(UserService userService) {
+    public CompanyService(CrudRepository<Integer, Company> companyRepository,
+                          UserService userService){
+        this.companyRepository = companyRepository;
         this.userService = userService;
     }
+
+    public Optional<CompanyReadDto> findById(Integer id){
+        return companyRepository.findById(id)
+                .map(entity -> new CompanyReadDto(entity.id()));
+    }
+
 }
