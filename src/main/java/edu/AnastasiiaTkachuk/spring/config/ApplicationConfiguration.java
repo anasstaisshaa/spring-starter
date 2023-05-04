86c7911a75ab31222d3e4793da93716b64365088
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-05-04 18:47:23 +0300

    add new methods to ApplicationConfiguration with annotation Bean

--- src/main/java/edu/AnastasiiaTkachuk/spring/config/ApplicationConfiguration.java
+++ src/main/java/edu/AnastasiiaTkachuk/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,10 @@
 package edu.AnastasiiaTkachuk.spring.config;
 
+import edu.AnastasiiaTkachuk.spring.database.pool.ConnectionPool;
 import edu.AnastasiiaTkachuk.spring.database.repository.CrudRepository;
+import edu.AnastasiiaTkachuk.spring.database.repository.UserRepository;
 import edu.AnastasiiaTkachuk.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -16,4 +19,13 @@ import org.springframework.stereotype.Component;
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "edu\\..+Repository")
         })
 public class ApplicationConfiguration {
+        @Bean
+        public ConnectionPool pool2(@Value("${db.username}") String username){
+                return new ConnectionPool(username, 20);
+        }
+
+        @Bean
+        public UserRepository userRepository2(ConnectionPool pool2){
+                return new UserRepository(pool2);
+        }
 }
